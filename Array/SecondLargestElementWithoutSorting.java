5cb2a9e0eb6f99175275c1f76b2332d983ff1e43
Prachi Marathe <167339270+prachimarathe2004@users.noreply.github.com>
2025-07-26 10:34:59 +0530

    Update SecondLargestElementWithoutSorting.java

--- Array/SecondLargestElementWithoutSorting.java
+++ Array/SecondLargestElementWithoutSorting.java
@@ -1,8 +1,8 @@
-public class SecondLargestElementWithoutSorting {
+public class SecondLargestWithoutSorting {
 
     public static int findSecondLargest(int[] arr) {
         if (arr.length < 2) {
-            System.out.println("Array should have at least two elements.");
+            System.out.println("Array must have at least two elements.");
             return -1;
         }
 
@@ -13,13 +13,13 @@ public class SecondLargestElementWithoutSorting {
             if (num > largest) {
                 secondLargest = largest;
                 largest = num;
-            } else if (num > secondLargest && num != largest) {
+            } else if (num > secondLargest && num < largest) {
                 secondLargest = num;
             }
         }
 
         if (secondLargest == Integer.MIN_VALUE) {
-            System.out.println("No second largest element found (all elements might be equal).");
+            System.out.println("No second largest element found (all elements might be equal).\n");
             return -1;
         }
 
@@ -27,7 +27,7 @@ public class SecondLargestElementWithoutSorting {
     }
 
     public static void main(String[] args) {
-        int[] arr = {12, 35, 1, 10, 34, 1};
+        int[] arr = {22,33,33,56,78,78,59,4}; // second largest should be 3
         int result = findSecondLargest(arr);
         if (result != -1) {
             System.out.println("Second largest element is: " + result);
