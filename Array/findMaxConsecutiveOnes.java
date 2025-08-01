9735662e457e95245c66bc67b8458319a7fcb277
Prachi Marathe <167339270+prachimarathe2004@users.noreply.github.com>
2025-08-01 12:28:21 +0530

    Create findMaxConsecutiveOnes.java

---
+++ Array/findMaxConsecutiveOnes.java
@@ -0,0 +1,25 @@
+public class MaxConsecutiveOnes {
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int maxCount = 0;
+        int currentCount = 0;
+
+        for (int num : nums) {
+            if (num == 1) {
+                currentCount++;
+                maxCount = Math.max(maxCount, currentCount);
+            } else {
+                currentCount = 0;
+            }
+        }
+
+        return maxCount;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 0, 1, 1, 1, 0, 1, 1};
+
+        int result = findMaxConsecutiveOnes(nums);
+        System.out.println("Maximum number of consecutive 1s: " + result);
+    }
+}
