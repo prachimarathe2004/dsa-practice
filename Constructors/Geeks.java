517be75393f3c31e3b052fcc751f186a725bdf68
Prachi Marathe <167339270+prachimarathe2004@users.noreply.github.com>
2025-12-15 18:56:13 +0530

    Add default constructor in Geeks class

---
+++ Constructors/Geeks.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class Geeks{
+
+    // Default Constructor
+    Geeks(){
+        
+        System.out.println("Default constructor"); 
+        
+    }
+    public static void main(String[] args){
+        
+        Geeks hello = new Geeks();
+    }
+}
